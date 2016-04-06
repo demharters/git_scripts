99385d4d1e2a3a07e62dd7537f0b5d09e2bc6b4a
demharters <samuel.demharter@seh.ox.ac.uk>
2016-04-06 14:35:54 +0100

    sdffds

--- dist_chm.py
+++ dist_chm.py
@@ -40,18 +40,39 @@ v = Universe(my_traj)
 end = my_traj.find('.pdb')
 fout_name = my_traj[0:end] + '_dist.dat'
 
-o6_1 = u.selectAtoms("segid A and resid 9 and name O6")
-chm_1 = u.selectAtoms("segid A and resid 8 and name HO")
-o6_2 = u.selectAtoms("segid B and resid 19 and name O6")
-chm_2 = u.selectAtoms("segid B and resid 18 and name HO")
+#o6_1 = u.selectAtoms("segid A and resid 9 and name O6")
+#chm_1 = u.selectAtoms("segid A and resid 8 and name HO")
+#o6_2 = u.selectAtoms("segid B and resid 19 and name O6")
+#chm_2 = u.selectAtoms("segid B and resid 18 and name HO")
+
+A9_1 = u.selectAtoms("segid A and resid 9 and name O4'")
+A8_1 = u.selectAtoms("segid A and resid 8 and name O4'")
+A9_2 = u.selectAtoms("segid A and resid 9 and name C1'")
+A8_2 = u.selectAtoms("segid A and resid 8 and name C1'")
+A9_3 = u.selectAtoms("segid A and resid 9 and name C2'")
+A8_3 = u.selectAtoms("segid A and resid 8 and name C2'")
+A9_4 = u.selectAtoms("segid A and resid 9 and name C3'")
+A8_4 = u.selectAtoms("segid A and resid 8 and name C3'")
+A9_5 = u.selectAtoms("segid A and resid 9 and name C4'")
+A8_5 = u.selectAtoms("segid A and resid 8 and name C4'")
+
+A9_ring = u.selectAtoms("segid A and resid 9 and (name O4' or name C1' or name C2' or name C3' or name C4')")
+A8_ring = u.selectAtoms("segid A and resid 8 and (name O4' or name C1' or name C2' or name C3' or name C4')")
 
 f = open(fout_name,'w')
 
 for ts in u.trajectory:
         
-    distance1 = numpy.linalg.norm(o6_1.centerOfMass() - chm_1.centerOfMass())
-    distance2 = numpy.linalg.norm(o6_2.centerOfMass() - chm_2.centerOfMass())
+    #distance1 = numpy.linalg.norm(o6_1.centerOfMass() - chm_1.centerOfMass())
+    #distance2 = numpy.linalg.norm(o6_2.centerOfMass() - chm_2.centerOfMass())
     
-    f.write('%7.3f\t%7.3f\n' % (distance1,distance2))
+    distance1 = numpy.linalg.norm(A9_1.centerOfMass() - A8_1.centerOfMass())
+    distance2 = numpy.linalg.norm(A9_2.centerOfMass() - A8_2.centerOfMass())
+    distance3 = numpy.linalg.norm(A9_3.centerOfMass() - A8_3.centerOfMass())
+    distance4 = numpy.linalg.norm(A9_4.centerOfMass() - A8_4.centerOfMass())
+    distance5 = numpy.linalg.norm(A9_5.centerOfMass() - A8_5.centerOfMass())
+    distance6 = numpy.linalg.norm(A9_ring.centerOfMass() - A8_ring.centerOfMass())
+    
+    f.write('%7.3f\t%7.3f\t%7.3f\t%7.3f\t%7.3f\t%7.3f\n' % (distance1,distance2,distance3,distance4,distance5,distance6))
 f.close()
 
